81396313a8203f30520ac0d36c8680e5ed2d6c30
kaim-xotiv <m.kaim@xotiv.com>
2025-05-19 21:33:22 +0530

    Revert "chore: push latest changes"
    This reverts commit 37b20b644a42fbd1bbc5b9fe044610ada1b55717.

--- terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-#terraform {
- # backend "s3" {
-  #  bucket         = "terraform-backend-prabhat"
-   # key            = "smc-react-app/terraform.tfstate"
-    #region         = "ap-south-1"
-    #encrypt        = true
-  #}
-#}
